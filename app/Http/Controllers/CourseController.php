da5fcf5ccf504d865a7847601d78b8a1371a2b03
chiefhaven <chikumbahappy@gmail.com>
2023-04-23 15:03:48 +0200

    Update CourseController.php

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -14,6 +14,7 @@ class CourseController extends Controller
     public function __construct()
     {
         $this->middleware(['role:superAdmin']);
+        $this->middleware(['role:admin']);
     }
 
     /**
