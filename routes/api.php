f2905bca3e68c3ee443ea1477eef34e1f8386700
chiefhaven <chikumbahappy@gmail.com>
2023-06-05 23:16:21 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::post('login', [AuthController::class, 'login'])->name('login.api'); // Lo
 Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 
 Route::get('invoicesHome', [HomeController::class],'index')->middleware('auth');
-Route::get('invoices', [InvoiceController::class, 'index'])->middleware('auth')->name('invoices');
+Route::get('/invoices', [InvoiceController::class, 'index'])->middleware('auth');
 Route::get('invoice-view/{id}', [InvoiceController::class, 'show'])->middleware('auth');
 
 
