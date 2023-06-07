05f49d4343eab759a837c5dcfbf509d5d2c8dcfb
chiefhaven <chikumbahappy@gmail.com>
2023-06-07 14:39:25 +0200

    Update header.blade.php

--- resources/views/pdf_templates/partials/header.blade.php
+++ resources/views/pdf_templates/partials/header.blade.php
@@ -13,16 +13,16 @@
         <p>
             @if(isset($student->fname))
                 {{ $watermak = $student->fname.' '.$student->mname.' '.$student->sname }}
-                @for($i = 0; $i < 1000; $i++)
+                @for($i = 0; $i < 1500; $i++)
                     {{ $watermak }}
                 @endfor
             @elseif(isset($invoice->student->fname))
                 {{ $watermak = $invoice->student->fname.' '.$invoice->student->mname.' '.$invoice->student->sname }}
-                @for($i = 0; $i < 1000; $i++)
+                @for($i = 0; $i < 1500; $i++)
                     {{$watermak}}
                 @endfor
             @else
-                @for($i = 0; $i < 1000; $i++)
+                @for($i = 0; $i < 1500; $i++)
                     Driving School Management System
                 @endfor
             @endif
