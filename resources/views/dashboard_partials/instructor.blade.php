a48e0a656e96e031f6da311f6103fdefebf22c95
Chief Haven <chikumbahappy@gmail.com>
2024-06-05 03:22:54 +0200

    Update instructor.blade.php

--- resources/views/dashboard_partials/instructor.blade.php
+++ resources/views/dashboard_partials/instructor.blade.php
@@ -1,5 +1,15 @@
 <div class="row">
-        <div class="col-md-4 col-xl-4">
+        <div class="col-md-6 col-xl-6">
             <h1>Welcome <b>{{ Auth::user()->instructor->fname }}</b> {{ Auth::user()->instructor->sname }}</h1>
         </div>
+
+</div>
+
+<div class="block-content">
+    <div class="row">
+        <div class="col-md-8 col-xl-8 card p-5 mt-6">
+            {!! \Illuminate\Foundation\Inspiring::quote() !!}
+        </div>
+    </div>
+
 </div>
