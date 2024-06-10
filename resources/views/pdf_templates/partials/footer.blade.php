b25fa69d5e2404699eb89026574610441ff3af58
Chief Haven <chikumbahappy@gmail.com>
2024-06-10 22:54:08 +0200

    Update footer.blade.php

--- resources/views/pdf_templates/partials/footer.blade.php
+++ resources/views/pdf_templates/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<p>Your assistance rendered to 
+<p>Your assistance rendered to
         @if($student->gender == "Male")
             him
         @elseif($student->gender == "Female")
@@ -8,12 +8,11 @@
         @endif
      will be highly appreciated.<br><br>
 
-    Yours Faithfully<br>    
+    Yours Faithfully<br>
     <img src="{{ public_path("media/signatures/{$setting->authorization_signature}") }}" alt="" style="width: auto; height: 20px;"><br>
     <b>Chimwemwe Mboma</b><br>Director<br>Cell: +265 999 532 688/884 511 827<br>Email: chimwemwemboma@darondrivingschool.com</p>
 </div>
 </div>
 </div>
-
 </body>
 </html>
