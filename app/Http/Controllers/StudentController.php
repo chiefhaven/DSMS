5f6b47b8c39f83d3cb4ab161ccb0649b09170717
Chief Haven <chikumbahappy@gmail.com>
2024-06-11 10:47:33 +0200

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -253,7 +253,7 @@ class StudentController extends Controller
         $student = Student::find($id);
         $setting = Setting::find(1);
         $date = date('j F, Y');
-        $qrCode = QrCode::size(120)->backgroundColor(255, 0, 0)->generate('https://darondrivingschool.com/');
+        $qrCode = QrCode::backgroundColor(255, 0, 0)->generate('https://darondrivingschool.com/');
 
         $pdf = PDF::loadView('pdf_templates.trafficCardReferenceLetter', compact('student', 'setting', 'date', 'qrCode'));
         return $pdf->download('Daron Driving School-'.$student->fname.' '.$student->sname.' Trafic Card Reference Letter.pdf');
