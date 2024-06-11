9ae9e0f2320f41cc97027217f2d98652c0b66777
Chief Haven <chikumbahappy@gmail.com>
2024-06-11 21:36:41 +0200

    Update qrcode.blade.php

--- resources/views/pdf_templates/partials/qrcode.blade.php
+++ resources/views/pdf_templates/partials/qrcode.blade.php
@@ -1,3 +1,3 @@
-<div class="" style="position:absolute; bottom:10%; right:0%; height: 150px; width:200px;;">
+<div class="" style="position:absolute; bottom:5%; right:0%; height: 150px; width:200px;;">
     <img src="data:image/png;base64, {!! $qrCode !!} ">
 </div>
