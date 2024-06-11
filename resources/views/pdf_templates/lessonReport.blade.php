ef16f22154e6bd41c2c52247c2e72f9460c58a70
Chief Haven <chikumbahappy@gmail.com>
2024-06-11 21:06:57 +0200

    Update lessonReport.blade.php

--- resources/views/pdf_templates/lessonReport.blade.php
+++ resources/views/pdf_templates/lessonReport.blade.php
@@ -8,7 +8,7 @@
 @include('pdf_templates.partials.header')
 @include('pdf_templates.partials.watermark')
 
-<div class="container" style="left:50px; padding-left:20px">
+<div class="container" style="left:70px; padding-left:20px">
     <div class="row">
         <div class="container">
             <div class="col-lg-12">
