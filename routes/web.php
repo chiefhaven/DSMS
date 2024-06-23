f53a047ab7e4fba30be2503068b2953b8518c60c
Chief Haven <chikumbahappy@gmail.com>
2024-06-23 17:54:52 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -146,7 +146,7 @@ Auth::routes();
 
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth'])->name('home');
 
-Route::get('/migrate', function(){
-    Artisan::call('migrate',['--force' => true]);
-     dd('migrated!');
- });
+// Route::get('/migrate', function(){
+//     Artisan::call('migrate',['--force' => true]);
+//      dd('migrated!');
+//  });
