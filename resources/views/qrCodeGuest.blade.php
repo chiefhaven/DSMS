0d8c50ec3c43df803a8cac2a82274a5074687fd5
Chief Haven <chikumbahappy@gmail.com>
2024-06-23 23:25:13 +0200

    Update qrCodeGuest.blade.php

--- resources/views/qrCodeGuest.blade.php
+++ resources/views/qrCodeGuest.blade.php
@@ -1,18 +1,19 @@
 <x-guest-layout>
-    <x-slot name="logo">
-        <h1 style="font-size: 2.7em; text-align:center; color:aliceblue"><strong>{{ env('APP_NAME')}}</strong></h1>
-</x-slot>
-    <div class="block-content">
+<div class="block-content">
     <div class="content">
-        <p class="text-uppercase">This is a Daron Driving School document, <br>and contains details of one of our students!</p>
-    <p>
-        For more information contact us on <br>
-        Phone: +265 999 532 688 | +265 887 226 317<br>
-        Email: info@darondrivingschool.com
-    </p>
+        <div class="text-uppercase">
+            @if(isset($student))
+                This is a valid document for {{ $student->fname }} {{ $student->mname }} {{ $student->sname }}!
+            @else
+                Document or url not valid!
+            @endif
+        </div>
+        <div>
+            <hr>
+            For more information contact us on <br>
+            Phone: +265 999 532 688 | +265 887 226 317<br>
+            Email: info@darondrivingschool.com
+        </div>
     </div>
 </div>
-<x-slot name="logo">
-    <h1 style="font-size: 2.7em; text-align:center; color:aliceblue"><strong>{{ env('APP_NAME')}}</strong></h1>
-</x-slot>
 </x-guest-layout>
