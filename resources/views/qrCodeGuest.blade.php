12512995a6c4dd498802f85a1ff8e55b3d55ed04
Chief Haven <chikumbahappy@gmail.com>
2024-06-23 19:13:28 +0200

    Create qrCodeGuest.blade.php

---
+++ resources/views/qrCodeGuest.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.guest')
+
+@section('content')
+<div class="block-content">
+    <div class="content">
+        <p class="text-uppercase">This is a Daron Driving School document, <br>and contains details of one of our students!</p>
+    <p>
+        For more information contact us on <br>
+        Phone: +265 999 532 688 | +265 887 226 317<br>
+        Email: info@darondrivingschool.com
+    </p>
+    </div>
+</div>
+
+@endsection
