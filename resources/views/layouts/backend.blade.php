5d41b6118ed711dd61d9d7a0c7018a4dd50b196d
Chief Haven <chikumbahappy@gmail.com>
2024-07-12 23:08:23 +0200

    Update backend.blade.php

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -58,6 +58,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
   <script src="https://unpkg.com/vuejs-paginate-next@latest/dist/vuejs-paginate-next.umd.js"></script>
   <script type="text/javascript" src="https://unpkg.com/@zxing/library@latest"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
   <script>
