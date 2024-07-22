74112cf225ac6be7460c23453e5fe03fb0bb3119
Chief Haven <chikumbahappy@gmail.com>
2024-07-22 15:06:49 +0200

    Update 2024_07_15_124409_create_expense_student_table.php

--- database/migrations/2024_07_15_124409_create_expense_student_table.php
+++ database/migrations/2024_07_15_124409_create_expense_student_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('expense_student', function (Blueprint $table) {
             $table->id();
-            $table->uuid('expense_id');
-            $table->uuid('student_id');
+            $table->foreignUuid('expense_id');
+            $table->foreignUuid('student_id');
             $table->enum('expense_type', ['TRN', 'Highway Code I', 'Highway Code II','Road Test']);
 
             $table->foreign('expense_id')->references('id')
