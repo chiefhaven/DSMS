97f4c0289de150e73323b03cdeef94edb60c2a61
Chief Haven <chikumbahappy@gmail.com>
2024-07-24 00:22:02 +0200

    Update letter_head.blade.php

--- resources/views/pdf_templates/partials/letter_head.blade.php
+++ resources/views/pdf_templates/partials/letter_head.blade.php
@@ -1,10 +1,3 @@
-<div class="" style="position:absolute; bottom:25%; left:0%; height: 150px; width:200px;;">
-    <img src="{{ public_path("media/mboma.jpg") }}" alt="Sign here" style="width: 100%; height: auto;">
-</div>
-
-<div class="" style="position:absolute; bottom:0%; right:-10%; height: 150px; width:200px;;">
-    <img src="data:image/png;base64, {!! $qrCode !!} ">
-</div>
 <div class="row" style="margin: 0 !important">
     <div class="col-12" style="margin: 0 !important">
     <table class="table">
