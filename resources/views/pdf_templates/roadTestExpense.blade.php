9a0909f548c115972c811b05145ee38b81145cb8
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 10:08:31 +0200

    Update roadTestExpense.blade.php

--- resources/views/pdf_templates/roadTestExpense.blade.php
+++ resources/views/pdf_templates/roadTestExpense.blade.php
@@ -13,29 +13,31 @@
 
     <h3 style="text-transform: uppercase;"><u>APPLICATION FOR A ROAD TEST</u></h3>
 
-    <P>I write to seek your assistance for the following students to take their road test.</P><p> </p>
-    <table class="table table-striped table-responsive" style="font-size:12px; background-color: #ffffff !important">
-        <thead style="color: #ffffff !important; background-color:#0665d0; text-align:left !important">
-            <th class="invoice-td" style="text-align:left !important">Student</th>
-            <th class="invoice-td" style="text-align:left !important">Class</th>
-            <th class="invoice-td" style="text-align:left !important">Veihcle Reg Number</th>
-        </thead>
-        <tbody>
-            @foreach ($expense->students as $student)
-                <tr class="py-1" style="padding-top: 0px; padding-bottom: 0px; ">
-                    <td class="invoice-td text-uppercase">
-                        {{$student->fname}} {{$student->mname}} {{$student->sname}}
-                    </td>
-                    <td class="invoice-td">
-                        {{$student->course->class}}
-                    </td>
-                    <td class="invoice-td">
-                        {{$student->fleet->car_registration_number}}
-                    </td>
-                </tr>
-                @endforeach
-        </tbody>
-    </table>
+    <P>I write to seek your assistance for the following students to take their road test.</P><p></p>
+    <div class="bg-body" style="z-index:999 !important">
+        <table class="table table-striped table-responsive" style="font-size:12px;">
+            <thead style="color: #ffffff !important; background-color:#0665d0; text-align:left !important">
+                <th class="invoice-td" style="text-align:left !important">Student</th>
+                <th class="invoice-td" style="text-align:left !important">Class</th>
+                <th class="invoice-td" style="text-align:left !important">Veihcle Reg Number</th>
+            </thead>
+            <tbody>
+                @foreach ($expense->students as $student)
+                    <tr class="py-1" style="padding-top: 0px; padding-bottom: 0px; ">
+                        <td class="invoice-td text-uppercase">
+                            {{$student->fname}} {{$student->mname}} {{$student->sname}}
+                        </td>
+                        <td class="invoice-td">
+                            {{$student->course->class}}
+                        </td>
+                        <td class="invoice-td">
+                            {{$student->fleet->car_registration_number}}
+                        </td>
+                    </tr>
+                    @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @include('pdf_templates.partials.groupFooter')
