41c77a1991c80872d00f853eed42dc43424e3ba2
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 17:18:14 +0200

    Update system.blade.php

--- resources/views/settings/partials/system.blade.php
+++ resources/views/settings/partials/system.blade.php
@@ -25,13 +25,30 @@
           <div class="row haven-floating">
             <div class="mb-4 col-md-6 form-floating">
                 <input type="number" class="form-control @error('fees_threshold') is-invalid @enderror" id="fees_threshold" name="fees_threshold" value="{{$setting->fees_balance_threshold}}" placeholder="Lessons threshold (%)">
-                <label class="form-label" for="fees_threshold">Fees balance threshold (%)</label>
+                <label class="form-label" for="fees_threshold">Fees attendance threshold (%)</label>
                 <p class="muted small"><em>Fees percent at which student is allowed to continue with attending lessons</em></p>
             </div>
             <div class="mb-4 col-md-6 form-floating">
                 <input type="number" class="form-control @error('lesson_threshold') is-invalid @enderror" id="lesson_threshold" name="lesson_threshold" value="{{$setting->attendance_threshold}}" placeholder="Lessons threshold (%)">
                 <label class="form-label">Lessons threshold (%)</label>
                 <p class="muted small"><em>Attendance percent at which student is allowed to continue with attending lessons when they have balance</em></p>
+            </div>
+          </div>
+          <div class="row haven-floating">
+            <div class="mb-4 col-md-4 form-floating">
+                <input type="number" class="form-control @error('fees_code_i_threshold') is-invalid @enderror" id="fees_code_i_threshold" name="fees_code_i_threshold" value="{{$setting->fees_code_i_threshold}}" placeholder="Highway code I threshold (%)">
+                <label class="form-label" for="fees_threshold">Highway code I threshold (%)</label>
+                <p class="muted small"><em>Fees percent at which student can be selected for highway code I expense</em></p>
+            </div>
+            <div class="mb-4 col-md-4 form-floating">
+                <input type="number" class="form-control @error('fees_code_ii_threshold') is-invalid @enderror" id="fees_code_ii_threshold" name="fees_code_ii_threshold" value="{{$setting->fees_code_ii_threshold}}" placeholder="Highway code II threshold (%)">
+                <label class="form-label" for="fees_threshold">Highway code II threshold (%)</label>
+                <p class="muted small"><em>Fees percent at which student can be selected for highway code II expense</em></p>
+            </div>
+            <div class="mb-4 col-md-4 form-floating">
+                <input type="number" class="form-control @error('fees_road_test_threshold') is-invalid @enderror" id="fees_road_test_threshold" name="fees_road_test_threshold" value="{{$setting->fees_road_threshold}}" placeholder="Road Test threshold (%)">
+                <label class="form-label" for="fees_threshold">Road Test threshold (%)</label>
+                <p class="muted small"><em>Fees percent at which student can be selected for road test expense</em></p>
             </div>
           </div>
           <br>
