ab167d73145d832916c73cc49dce45c2ee988940
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 10:59:38 +0200

    Update header.blade.php

--- resources/views/pdf_templates/partials/header.blade.php
+++ resources/views/pdf_templates/partials/header.blade.php
@@ -1,10 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<div class="" style="height: 300%; width: 5px; position:absolute; top:-90px; left: 30px; z-index: 999; background: blue;">
-    <p>&nbsp;</p>
+<div class="blue-line-left">
 </div>
-<div class="" style="height: 300%; width: 2px; position:absolute; top:-90px; left: 35px; z-index: 999; background: black;">
-    <p>&nbsp;</p>
+<div class="black-line">
 </div>
-<div class="" style="height: 300%; width: 3px; position:absolute; top:-90px; left: 37px; z-index: 999; background: blue;">
-    <p>&nbsp;</p>
+<div class="blue-line-right">
 </div>
