350347311325f3acc25ad833258dd4125c6ed725
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 13:13:38 +0200

    Update header.blade.php

--- resources/views/pdf_templates/partials/header.blade.php
+++ resources/views/pdf_templates/partials/header.blade.php
@@ -1,7 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<div class="blue-line-left">
-</div>
-<div class="black-line">
-</div>
-<div class="blue-line-right">
-</div>
+<blue-line-left>
+</blue-line-left>
+<black-line>
+</black-line>
+<blue-line-right>
+</blue-line-right>
