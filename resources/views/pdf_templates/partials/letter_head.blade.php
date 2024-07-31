967c6e84ab64589b2d240ed9a17ec600f36eed5b
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 13:13:49 +0200

    Update letter_head.blade.php

--- resources/views/pdf_templates/partials/letter_head.blade.php
+++ resources/views/pdf_templates/partials/letter_head.blade.php
@@ -18,9 +18,6 @@
             </tr>
         </tbody>
     </table>
-    <div class="" style="position:absolute; bottom:65%; right: 0%; height: 150px; width:200px;;">
-        <img src="data:image/png;base64, {!! $qrCode !!} ">
-    </div>
 </div>
 </div>
 <p>Dear Sir/Madam,</p>
