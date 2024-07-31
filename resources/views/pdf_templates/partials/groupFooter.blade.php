bf1ab9adfadf32a618f41ab5c571aaa350cbfcfa
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 09:45:30 +0200

    Update groupFooter.blade.php

--- resources/views/pdf_templates/partials/groupFooter.blade.php
+++ resources/views/pdf_templates/partials/groupFooter.blade.php
@@ -3,9 +3,6 @@
     Yours Faithfully<br>
     <img src="{{ public_path("media/signatures/{$setting->authorization_signature}") }}" alt="" style="width: auto; height: 20px;"><br>
     <b>Chimwemwe Mboma</b><br>Director<br>Cell: +265 999 532 688/884 511 827<br>Email: chimwemwemboma@darondrivingschool.com</p>
-    <div class="" style="position:absolute; bottom:65%; right:-10%; height: 150px; width:200px;;">
-        <img src="data:image/png;base64, {!! $qrCode !!} ">
-    </div>
     @include('pdf_templates.partials.id')
 </div>
 </div>
