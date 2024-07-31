67733bf1883124b27c55f143ceb3f0cdc3468c78
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 13:13:43 +0200

    Update roadTestExpense.blade.php

--- resources/views/pdf_templates/roadTestExpense.blade.php
+++ resources/views/pdf_templates/roadTestExpense.blade.php
@@ -8,6 +8,7 @@
 @include('pdf_templates.partials.header')
 @include('pdf_templates.partials.watermark')
 @include('pdf_templates.partials.pdf_template_style')
+@include('pdf_templates.partials.groupHeader')
 @include('pdf_templates.partials.letter_head')
 
 
