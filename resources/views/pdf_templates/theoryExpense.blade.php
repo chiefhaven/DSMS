0ad7b785bf7b107d5f17585458bb0898ea69fe36
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 13:13:46 +0200

    Update theoryExpense.blade.php

--- resources/views/pdf_templates/theoryExpense.blade.php
+++ resources/views/pdf_templates/theoryExpense.blade.php
@@ -9,6 +9,7 @@
 @include('pdf_templates.partials.watermark')
 @include('pdf_templates.partials.pdf_template_style')
 @include('pdf_templates.partials.letter_head')
+@include('pdf_templates.partials.groupHeader')
 
 
     <h3 style="text-transform: uppercase;"><u>APPLICATION FOR STUDENT’S APTITUDE TEST</u></h3>
@@ -17,7 +18,7 @@
     <table class="table table-striped table-responsive" style="font-size:12px; background-color: #ffffff !important; overflow:visible">
         <thead style="color: #ffffff !important; background-color:#0665d0; text-align:left !important;">
             <th class="invoice-td" style="text-align:left !important">Student name</th>
-            <th class="invoice-td" style="text-align:left !important">Class</th>
+            <th class="invoice-td">Class</th>
             <th class="invoice-td">Highway code I</th>
             <th class="invoice-td">Highway code II</th>
         </thead>
@@ -27,19 +28,19 @@
                     <td class="invoice-td text-uppercase">
                         {{$student->fname}} {{$student->mname}} {{$student->sname}}
                     </td>
-                    <td class="invoice-td">
+                    <td class="invoice-td text-center">
                             @if (!$student->course)
                                 -
                             @else
                                 {{$student->course->class}}
                             @endif
                     </td>
-                    <td class="invoice-td" style="text-align:center !important">
+                    <td class="invoice-td text-center">
                         @if ($student->pivot->expense_type == 'Highway Code I')
                             <div style="font-family: DejaVu Sans, sans-serif;">✔</div>
                         @endif
                     </td>
-                    <td class="invoice-td" style="text-align:center !important">
+                    <td class="invoice-td text-center">
                         @if ($student->pivot->expense_type == 'Highway Code II')
                             <div style="font-family: DejaVu Sans, sans-serif;">✔</div>
                         @endif
