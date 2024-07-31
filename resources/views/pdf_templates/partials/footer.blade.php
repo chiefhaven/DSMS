a10843f5c59c0d1dcf319f2d9a350c2486938a88
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-07-31 13:13:34 +0200

    Update footer.blade.php

--- resources/views/pdf_templates/partials/footer.blade.php
+++ resources/views/pdf_templates/partials/footer.blade.php
@@ -11,7 +11,7 @@
     Yours Faithfully<br>
     <img src="{{ public_path("media/signatures/{$setting->authorization_signature}") }}" alt="" style="width: auto; height: 20px;"><br>
     <b>Chimwemwe Mboma</b><br>Director<br>Cell: +265 999 532 688/884 511 827<br>Email: chimwemwemboma@darondrivingschool.com</p>
-    <div class="" style="position:absolute; bottom:0%; right:-10%; height: 150px; width:200px;;">
+    <div class="" style="position:absolute; bottom:15%; right:0%; height: 150px; width:200px;;">
         <img src="data:image/png;base64, {!! $qrCode !!} ">
     </div>
     @include('pdf_templates.partials.id')
