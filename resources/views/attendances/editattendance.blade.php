52f00c66aec427cd2c9cfdcc35a84e82deb94902
chiefhaven <chikumbahappy@gmail.com>
2024-12-12 19:54:47 +0200

    Update editattendance.blade.php

--- resources/views/attendances/editattendance.blade.php
+++ resources/views/attendances/editattendance.blade.php
@@ -8,7 +8,7 @@
         <h1 class="flex-grow-1 fs-3 fw-semibold my-2 my-sm-3">Edit Attendance</h1>
         <nav class="flex-shrink-0 my-2 my-sm-0 ms-sm-3" aria-label="breadcrumb">
           <ol class="breadcrumb">
-            
+
           </ol>
         </nav>
       </div>
@@ -36,7 +36,7 @@
           <div class="row">
           <form class="mb-5" action="/updateattendance" method="post" onsubmit="return true;">
             @csrf
-            <input type="text" name="attendance_id" id="attendance_id" value="{{$attendance->id}}" hidden> 
+            <input type="text" name="attendance_id" id="attendance_id" value="{{$attendance->id}}" hidden>
             <div class="form-floating mb-4">
               <input type="datetime-local" class="date form-control" id="date" name="date" value="{{$attendance->attendance_date}}">
               <label class="form-label" for="example-school-name-input-floating">Date</label>
@@ -44,7 +44,7 @@
             <div class="form-floating mb-4">
               <input class="form-control" id="student" name="student" type="text" value="{{$attendance->student->fname}} {{$attendance->student->mname}} {{$attendance->student->sname}}">
               <label for="student">Student</label>
-            </div>          
+            </div>
             <div class="form-floating mb-4">
               <select class="form-select" id="lesson" name="lesson">
                 @foreach ($lesson as $lesson)
@@ -58,7 +58,7 @@
                 <button type="submit" class="btn btn-primary">Save</button>
             </div>
           </form>
-        </div>  
+        </div>
           </div>
       </div>
     </div>
