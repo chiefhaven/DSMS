23526a7353b555aac80a2bedad954b5ac728d8bb
chiefhaven <chikumbahappy@gmail.com>
2025-02-18 01:59:45 +0200

    Update student.blade.php

--- resources/views/dashboard_partials/student.blade.php
+++ resources/views/dashboard_partials/student.blade.php
@@ -1,5 +1,9 @@
 <div class="row">
-        <div class="col-md-4 col-xl-4">
-            sdfsdfsdf
+        <div class="col-md-8 col-xl-8">
+            <p>
+                You might have reached here after <strong>password reset;</strong> Now go to your App; your <strong>email address</strong> and <strong>password</strong> you just created as your credentials.</p>
+            <p>
+                <button class="btn btn-primary">Launch App</button>
+            </p>
         </div>
 </div>
