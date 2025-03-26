23421596bd3cb07234b5a9cea1af6c445eabea59
chiefhaven <chikumbahappy@gmail.com>
2025-03-26 02:57:51 +0200

    Update viewinstructor.blade.php

--- resources/views/instructors/viewinstructor.blade.php
+++ resources/views/instructors/viewinstructor.blade.php
@@ -14,10 +14,15 @@
               </button>
               <div class="dropdown-menu dropdown-menu-end p-0">
                 <div class="p-2">
-                  <form method="POST" action="/edit-instructor/{{$instructor->id}}">
-                    {{ csrf_field() }}
-                    <button class="dropdown-item" type="submit">Edit profile</button>
-                  </form>
+                    <form method="GET" action="{{ url('/editinstructor', $instructor->id) }}">
+                        {{ csrf_field() }}
+                        <button class="dropdown-item" type="submit">Edit</button>
+                    </form>
+                    <form method="POST" action="{{ url('/deleteinstructor', $instructor->id) }}">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button class="dropdown-item delete-confirm" type="submit">Delete</button>
+                    </form>
                 </div>
               </div>
             </div>
@@ -27,43 +32,90 @@
     </div>
   </div>
 
-<div class="content content-full">
-    <div class="block block-rounded">
-      <ul class="nav nav-tabs nav-tabs-block" role="tablist">
-        <li class="nav-item">
-          <button class="nav-link active" id="instructor-details-tab" data-bs-toggle="tab" data-bs-target="#instructor-details" role="tab" aria-controls="instructor-details" aria-selected="true">
-            instructor Details
-          </button>
-        </li>
-        <li class="nav-item">
-          <button class="nav-link" id="invoices-tab" data-bs-toggle="tab" data-bs-target="#invoices" role="tab" aria-controls="invoices" aria-selected="false">
-            Invoices
-          </button>
-        </li>
-        <li class="nav-item">
-          <button class="nav-link" id="payments-tab" data-bs-toggle="tab" data-bs-target="#payments" role="tab" aria-controls="payments" aria-selected="false">
-            Payments
-          </button>
-        </li>
-      </ul>
-      <div class="block-content tab-content">
-        <div class="tab-pane fade active show" id="instructor-details" role="tabpanel" aria-labelledby="instructor-details-tab">
-          <div class="content content-full row">
-            <div class="col-6" style="background: #ffffff; margin: 0 10px; border-radius: 5px; border: thin solid #cdcdcd;">
-              <div class="py-6 px-4">
-                <img class="img-avatar img-avatar96 img-avatar-thumb" src="/../media/avatars/avatar2.jpg" alt="">
-                <h1 class="my-2">{{$instructor->fname}} {{$instructor->sname}}</h1>
-                <p>
-                    Address: {{$instructor->address}} <br>Phone: {{$instructor->phone}}<br>Email: {{$instructor->user->email}}
-                </p>
-              </div>
+    <div class="content content-full">
+        <div class="block block-rounded">
+            <ul class="nav nav-tabs nav-tabs-block" role="tablist">
+                <li class="nav-item">
+                <button class="nav-link active" id="instructor-details-tab" data-bs-toggle="tab" data-bs-target="#instructor-details" role="tab" aria-controls="instructor-details" aria-selected="true">
+                    Instructor details
+                </button>
+                </li>
+            </ul>
+            <div class="block-content pb-4">
+                <div class="tab-pane fade active show" id="instructor-details" role="tabpanel" aria-labelledby="instructor-details-tab">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="py-6 px-4"  style="background: #ffffff; border-radius: 0px; border: thin solid #cdcdcd;">
+                                <img class="img-avatar img-avatar96 img-avatar-thumb" src="/../media/avatars/avatar2.jpg" alt="">
+                                <h1 class="my-2">{{$instructor->fname}} {{$instructor->sname}}</h1>
+                                <p>
+                                    Address: {{$instructor->address}} <br>Phone: {{$instructor->phone}}<br>Email: {{$instructor->user->email}}
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="py-6 px-2"  style="background: #ffffff; border-radius: 0px; border: thin solid #cdcdcd;">
+                                <table class="table">
+                                    <thead>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><b>Assigned:</b></td>
+                                            <td>
+                                                @if($instructor->fleet)
+                                                    <p>
+                                                        <b>{{ $instructor->fleet->car_registration_number}}</b><br>
+                                                        {{ $instructor->fleet->car_brand_model}}<br>
+                                                    </p>
+                                                @endif
+                                                @if($instructor->classrooms)
+                                                    <p>
+                                                        @foreach ($instructor->classrooms as $classroom)
+                                                            <b>{{ $classroom->name }}</b><br>
+                                                            {{ $classroom->location }}
+                                                        @endforeach
+                                                    </p>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Active Students:</b></td>
+                                            <td>
+                                                {{ ($instructor->fleet && $instructor->fleet->student) || $instructor->classrooms ?
+                                                    ($instructor->fleet && $instructor->fleet->student ? $instructor->fleet->student->where('status', '!=', 'Finished')->count() : 0) +
+                                                    ($instructor->classrooms ? $instructor->classrooms->sum(function($classroom) {
+                                                        return $classroom->students ? $classroom->students->where('status', '!=', 'Finished')->count() : 0;
+                                                    }) : 0) : 'None' }}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Todays attendances:</b></td>
+                                            <td>{{ $instructor->attendances->count() ?? '0' }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="block block-rounded p-4">
+                    Assigned students
+                    <h6>Coming soon....</h6>
+                </div>
             </div>
-         </div>
+
+            <div class="col-md-6">
+                <div class="block block-rounded p-4">
+                    Latest attendances
+                    <h6>Coming soon....</h6>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
-</div>
 
 
 @endsection
