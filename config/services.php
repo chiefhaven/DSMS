ca8082090d7ec0ce2a47820e663ba6fcb498d6e8
chiefhaven <chikumbahappy@gmail.com>
2025-04-02 22:49:06 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'clickmobile' => [
+            'url' => env('CLICKMOBILE_API_URL'),
+            'token' => env('CLICKMOBILE_BEARER_TOKEN'),
+        ],
+
 ];
