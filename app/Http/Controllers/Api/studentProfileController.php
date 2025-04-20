797a2c98591467a12b3f5f4d9b46cd69a462b138
chiefhaven <chikumbahappy@gmail.com>
2025-04-20 23:34:56 +0200

    Update studentProfileController.php

--- app/Http/Controllers/Api/studentProfileController.php
+++ app/Http/Controllers/Api/studentProfileController.php
@@ -11,7 +11,7 @@ use App\Models\Course;
 
 use function PHPUnit\Framework\isEmpty;
 
-class studentProfileController extends Controller
+class StudentProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
