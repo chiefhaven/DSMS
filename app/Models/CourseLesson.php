a8d6ccad9bfa5407950bca66d28ce6eac160e51d
chiefhaven <chikumbahappy@gmail.com>
2025-04-20 23:34:50 +0200

    Update CourseLesson.php

--- app/Models/CourseLesson.php
+++ app/Models/CourseLesson.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class courseLesson extends Model
+class CourseLesson extends Model
 {
     use HasFactory;
 }
