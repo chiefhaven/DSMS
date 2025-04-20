41df0bc51b103e4cf3727bf8ccc4cf553726c133
chiefhaven <chikumbahappy@gmail.com>
2025-04-20 23:34:47 +0200

    Update Invoice_Setting.php

--- app/Models/Invoice_Setting.php
+++ app/Models/Invoice_Setting.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class invoice_setting extends Model
+class Invoice_Setting extends Model
 {
     use HasFactory;
 }
