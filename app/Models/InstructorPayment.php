1f27d01ada1920e8cd32935c9fd2b999bbc8494e
chiefhaven <chikumbahappy@gmail.com>
2025-04-21 02:28:17 +0200

    Create InstructorPayment.php

---
+++ app/Models/InstructorPayment.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+
+class InstructorPayment extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'instructor_payments';
+
+    protected $fillable = [
+        'instructor_id',
+        'attendances_count',
+        'pay_per_attendance',
+        'total_payment',
+        'payment_date',
+        'payment_month',
+        'status',
+    ];
+
+    protected $casts = [
+        'payment_date' => 'date',
+        'attendances_count' => 'integer',
+        'pay_per_attendance' => 'decimal:2',
+        'total_payment' => 'decimal:2',
+    ];
+
+    public function instructor()
+    {
+        return $this->belongsTo(Instructor::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('instructor_payment')
+            ->logOnlyDirty()
+            ->setDescriptionForEvent(fn(string $eventName) =>
+                "Instructor payment record was {$eventName}."
+            );
+    }
+}
