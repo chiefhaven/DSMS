886430412e2533f976344b6d497b7f1d1e6dabf2
chiefhaven <chikumbahappy@gmail.com>
2025-04-23 02:22:39 +0200

    Update studentProfileController.php

--- app/Http/Controllers/Api/studentProfileController.php
+++ app/Http/Controllers/Api/studentProfileController.php
@@ -59,11 +59,11 @@ class studentProfileController extends Controller
             return response()->json(['message' => 'Student not found'], 404);
         }
 
-        $theoryCount = $student->Course->lessons
+        $theoryCount = optional($student->Course?->lessons)
             ->where('department_id', 'd9b69664-b8ca-11ef-9fee-525400adf70e')
             ->sum(fn($lesson) => $lesson->pivot->lesson_quantity ?? 0);
 
-        $practicalCount = $student->Course->lessons
+        $practicalCount = optional($student->Course?->lessons)
             ->where('department_id', 'd9b6a9c9-b8ca-11ef-9fee-525400adf70e')
             ->sum(fn($lesson) => $lesson->pivot->lesson_quantity ?? 0);
 
