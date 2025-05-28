fd68f3f86a07211bb8caffb9c5b614fd4e05b8cb
chiefhaven <chikumbahappy@gmail.com>
2025-05-29 00:40:21 +0200

    Update notifications.blade.php

--- resources/views/notifications/notifications.blade.php
+++ resources/views/notifications/notifications.blade.php
@@ -89,9 +89,8 @@
             const markAsRead = async (notification) => {
                 NProgress.start();
                 try {
-                  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-                  await axios.patch(`/notifications/${notification.id}/read`, {
+                  await axios.post(`/notifications/${notification.id}/readApi`, {
                     redirect_url: notification.data.url
                   }, {
                     headers: {
