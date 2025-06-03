6d57df9a95d09baa608a57f232b19d22ca185f6e
chiefhaven <chikumbahappy@gmail.com>
2025-06-04 01:51:33 +0200

    Update expenses.blade.php

--- resources/views/expenses/expenses.blade.php
+++ resources/views/expenses/expenses.blade.php
@@ -108,7 +108,7 @@
                                         <td>
                                             @php
                                                 $dateRaw = trim(preg_replace('/[^0-9\/\-]/', '', $expense->group));
-                                                $formats = ['d/m/Y', 'd-m-Y', 'Y-m-d'];
+                                                $formats = ['Y-m-d'];
                                                 $formattedDate = 'Invalid date';
                                                 foreach ($formats as $format) {
                                                     try {
