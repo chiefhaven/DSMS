fdfddae9397ea10940e68dd259313347ddca2c52
chiefhaven <chikumbahappy@gmail.com>
2025-06-05 09:15:47 +0200

    Update reviewExpense.blade.php

--- resources/views/expenses/reviewExpense.blade.php
+++ resources/views/expenses/reviewExpense.blade.php
@@ -162,7 +162,7 @@
             });
 
             const formatDate = (dateString) => {
-                return dayjs(dateString, ['DD/MM/YYYY', 'YYYY-MM-DD']).format('DD MMM, YYYY');
+                return dayjs(dateString, ['D/M/YYYY', 'DD/MM/YYYY', 'YYYY-MM-DD']).format('DD MMM, YYYY');
             };
 
             const paymentMethodOptions = ref([
