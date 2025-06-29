abcb7afd6d0b1740d5e1fdda7380e3e39d89f822
chiefhaven <chikumbahappy@gmail.com>
2025-06-29 22:01:35 +0200

    Update expense.php

--- app/Models/expense.php
+++ app/Models/expense.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
-class expense extends Model
+class Expense extends Model
 {
     use HasFactory, HasUuids, LogsActivity;
 
