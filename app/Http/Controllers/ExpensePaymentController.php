36f68a82da3701ab26232eacf3392ecc3f7ed937
chiefhaven <chikumbahappy@gmail.com>
2025-07-18 07:20:44 +0200

    Fix typo in overpayment error message
    Corrected 'exxceed' to 'exceed' in the error message when the payment amount is greater than the remaining balance.

--- app/Http/Controllers/ExpensePaymentController.php
+++ app/Http/Controllers/ExpensePaymentController.php
@@ -180,7 +180,7 @@ class ExpensePaymentController extends Controller
 
             if ($request->amount > $expense->pivot->balance) {
                 Log::info("Overpayment attempt: Requested amount {$request->amount} exceeds balance {$expense->pivot->balance}");
-                abort(422, 'Amount must be equal or less than the remaining balance.');
+                abort(422, 'Amount must not exxceed the remaining balance.');
             }
 
             $expensePayment = new ExpensePayment();
